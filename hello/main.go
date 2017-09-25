6897f6ad6af9402b58f9724f4ddfc6fd43195821
Loh Siu Yin <siuyin@beyondbroadcast.com>
2017-09-25 12:09:33 +0800

    Added log println statements for rudimentary tracing.

--- hello/main.go
+++ hello/main.go
@@ -7,6 +7,7 @@ import (
 	"time"
 )
 
+// 010_OMIT
 func main() {
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
@@ -14,11 +15,15 @@ func main() {
 	})
 
 	http.HandleFunc("/req", func(w http.ResponseWriter, r *http.Request) {
+		log.Println("Request processing started")
 		time.Sleep(10 * time.Millisecond) // do some work
 		fmt.Fprintf(w, `<p>request executed
 			<p><a href="/">home</a>`)
+		log.Println("Request processing finished")
 	})
 
 	log.Fatal(http.ListenAndServe(":8080", nil))
 
 }
+
+// 020_OMIT
